af332984a59e5ee89d5ac3ad85dd4329a9f58aef
Yun531 <jaeyoongh315@gmail.com>
2024-10-03 14:25:33 +0900

    요구사항 2~4 ??

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,7 +1,13 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import http.util.HttpRequestUtils;
+import http.util.IOUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.nio.file.Files;
@@ -12,10 +18,12 @@ import java.nio.file.Paths;
 public class RequestHandler implements Runnable{
     private static final String WEBAPP_PATH = "webapp";
     Socket connection;
+    private final MemoryUserRepository userRepository;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
-    public RequestHandler(Socket connection) {
+    public RequestHandler(Socket connection, MemoryUserRepository userRepository) {
         this.connection = connection;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -26,30 +34,84 @@ public class RequestHandler implements Runnable{
             DataOutputStream dos = new DataOutputStream(out);
 
 
-            // 요구사항 1
-            String request = br.readLine();
+
+            byte[] body;
+            String request = br.readLine();                                 //start Line
             if(request == null){
                 return;
             }
 
             //요청 라인 파싱
             String[] requestTokens = request.split(" ");
-            String method = requestTokens[0];
-            String url = requestTokens[1];
+            String requestMethod = requestTokens[0];
+            String requestUrl = requestTokens[1];
 
             //루트 경로 처리
-            if(url.equals("/")){
-                url = "/index.html";
+            if(requestUrl.equals("/")){
+                requestUrl = "/index.html";
             }
+            String filePath = WEBAPP_PATH + requestUrl;             //쿼리 스트링 존재할 수 있음
 
-            String filePath = WEBAPP_PATH + url;
-            if(Files.exists(Paths.get(filePath))){                          //요청한 파일을 읽어 응답
-                byte[] body = Files.readAllBytes(Paths.get(filePath));
+            //회원 가입 페이지로 이동, 버튼 누르는 거니까 해당 위치에서 처리해주는 건 아닌가?
+//            if(requestUrl.equals("/user/form.html")){
+//                body = Files.readAllBytes(Paths.get(requestUrl));
+//                response200Header(dos, body.length);
+//                responseBody(dos, body);
+//            }
+
+            // 요구사항 2, GET 회원 가입
+            if(requestMethod.equals("GET") && requestUrl.equals("/user/signup")){
+                String queryString = requestUrl.split("\\?")[1];
+                Map<String, String> queryParams = HttpRequestUtils.parseQueryParameter(queryString);        //주어진 쿼리 문자열을 파싱해여 map 형태로 반환
+
+                String userId = queryParams.get("userId");
+                String password = queryParams.get("password");
+                String name = queryParams.get("name");
+                String email = queryParams.get("email");
+                User user = new User(userId, password, name, email);            //User 객체 생성
+
+                if (userRepository.findUserById(userId) == null){
+                    userRepository.addUser(user);
+                }
+
+                response302Header(dos, "/index.html");
+            } // 요구사항 3, POST 회원 가입
+            else if (requestMethod.equals("POST") && requestUrl.equals("/user/signup")) {
+                int requestContentLength = 0;
+
+                while (true) {                                              //해더 영역 스킵
+                    String headerLine = br.readLine();
+                    if (headerLine.equals("")) {
+                        break;
+                    }
+                    if (headerLine.startsWith("Content-Length")) {          //바디의 길이가 적혀 있는 영역
+                        requestContentLength = Integer.parseInt(headerLine.split(": ")[1]);
+                    }
+                }
+
+                String requestBody = IOUtils.readData(br, requestContentLength);                        //바디 정보 읽어 옴
+                Map<String, String> bodyParams = HttpRequestUtils.parseQueryParameter(requestBody);     //주어진 쿼리 문자열 파싱하여 map 형태로 변환
+
+
+                String userId = bodyParams.get("userId");
+                String password = bodyParams.get("password");
+                String name = bodyParams.get("name");
+                String email = bodyParams.get("email");
+                User user = new User(userId, password, name, email);
+
+                if (userRepository.findUserById(userId) == null) {
+                    userRepository.addUser(user);
+                }
+
+                response302Header(dos, "/index.html");
+            }  //요구사항 1  (로그인 후에 파읽을 요청하는 경우를 방지하기 위해 else로 묶어주었음
+            else if(Files.exists(Paths.get(filePath))){                      //요청한 파일을 읽어 응답
+                body = Files.readAllBytes(Paths.get(filePath));
                 response200Header(dos, body.length);
                 responseBody(dos, body);
             }
             else{                                                           //요청한 파일이 존재하지 않음
-                byte[] body = "404 Not Found".getBytes();                   //문자열을 바이트코드로 인코딩
+                body = "404 Not Found".getBytes();                   //문자열을 바이트코드로 인코딩
                 response404Header(dos, body.length);
                 responseBody(dos, body);
             }
@@ -75,6 +137,16 @@ public class RequestHandler implements Runnable{
             dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
